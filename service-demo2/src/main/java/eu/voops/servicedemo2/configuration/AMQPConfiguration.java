26aa161abd6ad7473bea5f7306a952f4cef4791d
Snorre Ledal <snorreledal@hotmail.no>
2023-12-04 15:23:49 +0100

    Changed the dockerfile to make the services to boot slower to make time for infrastructure to boot up first in docker compose and changed back to stable spring boot 3.1.5

--- service-demo2/src/main/java/eu/voops/servicedemo2/configuration/AMQPConfiguration.java
+++ service-demo2/src/main/java/eu/voops/servicedemo2/configuration/AMQPConfiguration.java
@@ -1,7 +1,9 @@
 package eu.voops.servicedemo2.configuration;
 
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.context.annotation.Configuration;
 
+@EnableRabbit
 @Configuration
 public class AMQPConfiguration {
 
