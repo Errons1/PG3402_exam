c80cb58f4ceb046a750e5fd5321b2d30922c600f
Snorre Ledal <snorreledal@hotmail.no>
2023-12-11 14:11:35 +0100

    Edited the Hash class to handle NoSuchAlgorithmException

--- authentication/src/main/java/eu/voops/authentication/Hash.java
+++ authentication/src/main/java/eu/voops/authentication/Hash.java
@@ -1,18 +1,26 @@
 package eu.voops.authentication;
 
+import lombok.extern.slf4j.Slf4j;
+
 import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
+@Slf4j
 public class Hash {
-    public static byte[] sha256(String password) throws NoSuchAlgorithmException {
+    public static byte[] sha256(String password) {
 //        Not the strongest nor the slowest for password. 
 //        Also know I need a salt in here.
 //        But this exam is for microservices not safety, might come back here later!
 //        TODO: Make this better if you have time
-        MessageDigest digest = MessageDigest.getInstance("SHA-256");
+        MessageDigest digest = null;
+        try {
+            digest = MessageDigest.getInstance("SHA-256");
+        } catch (NoSuchAlgorithmException e) {
+            log.error("Could not Hash password!");
+            e.printStackTrace();
+            return null;
+        }
         return digest.digest(password.getBytes(StandardCharsets.UTF_8));
-//        byte[] hash = digest.digest(password.getBytes(StandardCharsets.UTF_8));
-//        return new String(hash, StandardCharsets.UTF_8);
     }
 }
