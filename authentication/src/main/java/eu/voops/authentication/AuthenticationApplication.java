75913b83abbbda21e4d3035eec6026bca739ac6c
Snorre Ledal <snorreledal@hotmail.no>
2023-12-18 23:38:32 +0100

    Enable JPA repositories.

--- authentication/src/main/java/eu/voops/authentication/AuthenticationApplication.java
+++ authentication/src/main/java/eu/voops/authentication/AuthenticationApplication.java
@@ -3,7 +3,9 @@ package eu.voops.authentication;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@EnableJpaRepositories
 @EnableDiscoveryClient
 @SpringBootApplication
 public class AuthenticationApplication {
--- transfer/src/main/java/eu/voops/transfer/TransferApplication.java
+++ transfer/src/main/java/eu/voops/transfer/TransferApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+
 @EnableDiscoveryClient
 @SpringBootApplication
 public class TransferApplication {
